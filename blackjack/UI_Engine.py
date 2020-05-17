a20865c74b6729a1d8411403681f5b00c6a9e4c8
Garwin Pryce <garwin.pryce@gmail.com>
2020-05-16 21:16:08 -0500

    fixed card positions

--- blackjack/UI_Engine.py
+++ blackjack/UI_Engine.py
@@ -7,7 +7,6 @@ class UI_Engine(wx.Frame):
     deckPosition = wx.Rect(20,20,20,20)
     dealerPosition = wx.Rect(20,20,20,20)
     playerPosition = wx.Rect(20,20,20,20)
-    dc = ""
 
     def __init__(self, *args, **kw):
         super(UI_Engine, self).__init__(*args, **kw)
@@ -29,7 +28,7 @@ class UI_Engine(wx.Frame):
         self.Update()
         screenBox = self.Rect
         
-        self.deckPosition = screenBox
+        self.deckPosition = wx.Rect(screenBox.Position,screenBox.Size)
         self.deckPosition.y = int(float(screenBox.Height) * .75) 
         print(self.deckPosition.y)
         print(screenBox.width)
@@ -37,35 +36,40 @@ class UI_Engine(wx.Frame):
         self.deckPosition.Height = 100
         self.deckPosition.width = 100
 
-        self.dealerPosition = screenBox
+        self.dealerPosition = wx.Rect(screenBox.Position,screenBox.Size)
         self.dealerPosition.y = int(float(screenBox.Height) * .16) 
         print(self.dealerPosition.y)
         print(screenBox.width)
         self.dealerPosition.x =  int(float(screenBox.width) * .5)
         self.dealerPosition.Height = 100
         self.dealerPosition.width = 100
+
+        self.playerPosition = wx.Rect(screenBox.Position,screenBox.Size)
+        self.playerPosition.y = int(float(screenBox.Height) * .75) 
+        print(self.playerPosition.y)
+        print(screenBox.width)
+        self.playerPosition.x =  int(float(screenBox.width) * .5)
+        self.playerPosition.Height = 100
+        self.playerPosition.width = 100
         
 
     def OnEraseBackground(self, evt):
         """    Add a picture to the background    """
         # yanked from ColourDB.py
-        self.dc = evt.GetDC()
-
-        if not self.dc:
-            self.dc = wx.ClientDC(self)
+        dc = evt.GetDC()
+        if not dc:
+            dc = wx.ClientDC(self)
             rect = self.GetUpdateRegion().GetBox()
-            self.dc.SetClippingRect(rect)
-        self.dc.Clear()
+            dc.SetClippingRect(rect)
+        dc.Clear()
         bmp = wx.Bitmap("images\stock-photo-black-jack-gambling-table.jpg")
-        self.dc.DrawBitmap(bmp, 0, 0)
+        dc.DrawBitmap(bmp, 0, 0)
 
-        self.DrawAtPosition("images/gray_back.jpg",self.deckPosition)
-        self.DrawAtPosition("images/gray_back.jpg",self.dealerPosition)
-        card = wx.BitmapFromImage(wx.ImageFromBitmap(wx.Bitmap("images/3C.jpg")).Scale(100,100))
-        #card.Scale(wx.Size(200,100))
-        self.dc.DrawBitmap(card,55,35)   
+        self.DrawAtPosition(dc,"images/gray_back.jpg",self.deckPosition)
+        self.DrawAtPosition(dc,"images/gray_back.jpg",self.dealerPosition)
+        self.DrawAtPosition(dc,"images/gray_back.jpg",self.playerPosition)  
 
-    def DrawAtPosition(self,imageName,rect):
+    def DrawAtPosition(self,dc,imageName,rect):
         image = wx.BitmapFromImage(wx.ImageFromBitmap(wx.Bitmap(imageName)).Scale(rect.Size.width,rect.Size.height ))
         #card.Scale(wx.Size(200,100))
-        self.dc.DrawBitmap(image,rect.Position)
+        dc.DrawBitmap(image,rect.Position)
